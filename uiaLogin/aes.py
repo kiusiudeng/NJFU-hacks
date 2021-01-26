aeb3f6821b5d7a27bf504c7b61e3e9839b05a08c
Kyouya <kiusiudeng@gmail.com>
2021-01-26 22:36:28 +0800

    add: uia登录算法

---
+++ uiaLogin/aes.py
@@ -0,0 +1,26 @@
+import random
+from itertools import repeat
+from Crypto.Cipher import AES
+import base64
+
+def rds(num):
+    keymap = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
+    result = ''
+    for i in repeat(None, num):
+        result += keymap[random.randint(0, 47)]
+    return result
+
+def pad(s):
+    BLOCK_SIZE = 16
+    return s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+
+def encrypt(data,key,iv):
+    key = key.encode('utf-8')
+    iv = iv.encode('utf-8')
+    mode = AES.MODE_CBC
+    do = AES.new(key, mode, iv)
+    result = do.encrypt(pad(data).encode('utf-8'))
+    return base64.b64encode(result).decode('utf-8')
+
+def aesEnc(pwd,key):
+    return encrypt(rds(64)+pwd,key,rds(16))
